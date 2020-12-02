eec726d3c5361c20728668d84f64747ef5527fc8
Andrii Mykhailiv <49981524+KERNOR-GENTOR@users.noreply.github.com>
2020-12-02 20:57:24 +0200

    Simple test example

--- SoftServeCarMobile/__tests__/App-test.js
+++ SoftServeCarMobile/__tests__/App-test.js
@@ -8,7 +8,11 @@ import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-
+/*
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+*/
+test('2 + 3 = 5', () => {
+  expect(2+3).toBe(5);
 });
