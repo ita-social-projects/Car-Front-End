0382d9137a02a970543d20a955317af5c6b40fc0
dreamofmaks <dreamofmaks@ukr.net>
2020-12-10 15:38:00 +0200

    modified Chat Component

--- SoftServeCarMobile/src/components/Chat/Chat.tsx
+++ SoftServeCarMobile/src/components/Chat/Chat.tsx
@@ -6,55 +6,64 @@ import {
     View,
     Text,
     TextInput,
-    Button
+    Button,
+    StyleSheet
 } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
+class Chat extends React.Component{
 
+    constructor(props){
+        super(props);
 
-const hubConnection = new signalR.HubConnectionBuilder().withUrl('http://10.0.2.2:61658/chat').build();
+        this.state = {
+            message: null,
+            messages: [],
+            hubConnection: null
+        }
+    }
 
-hubConnection.start();
-const Chat = () => {
+    componentDidMount() {
+        const hubConnection = new signalR.HubConnectionBuilder().withUrl('http://10.0.2.2:61658/chat').build();
+        this.setState({hubConnection}, () => {
+            this.state.hubConnection.start().then(() => "Connection started!"); 
 
-
-        const [message, setMessage] = useState('');
-        const [arrr, setArr] = useState(Array<Element>());
-
-        const arr: Array<Element> = []
-        
-
-        
-        const onSubmit = () => {
-               
-                hubConnection.invoke("SendMessage", message);
-           };
-        
-       
-
-         useEffect(() => {
-             hubConnection.on("RecieveMessage", message => {
-                 console.log(message);
-                 //setMessage(message)
-                 arr.push(<SimpleMessage mes={message} />)
-                 setArr(arr);
-                 
-             })
+            hubConnection.on("RecieveMessage", receivedMessage => {
+                const text = receivedMessage;
+                const messages = this.state.messages.concat([text]);
+                this.setState( {messages} );
+            })
         });
+    }
 
-        return(
+        onSubmit = () => {
+            console.log("here");
+            console.log(this.state.message);
+            console.log(this.state.messages);
+        this.state.hubConnection.invoke("SendMessage", this.state.message);
+
+        this.setState({message: ''});
+   };
+
+   render() {
+       return (
+           <>
             <View>
-                <Text>
-                    Enter your message:
-                </Text>
-                <TextInput placeholder="type here" onChangeText={(message) => setMessage(message)}></TextInput>
-                
-                <Button onPress={onSubmit} title="Send" />
-                <View>{arrr.map(item => item)}</View>
-                
+                <TextInput value={this.state.message}
+                placeholder="type text here"
+                onChangeText={(message) => {this.setState({message: message})}} />
+                <Button onPress={this.onSubmit} title="Send" />
+                {this.state.messages.map((message: string, index: number) => {
+                    return (
+                    <Text key={index}>{message}</Text>
+                    );
+                })}
             </View>
-        );
-    
+           </>
+       );
+   }
+
+
 }
 
 export default Chat;
