f61ad0231e37dc77ac856014afca5a32aa7cca02
unknown <kotsun.nazar69@gmail.com>
2021-01-17 15:10:34 +0200

    Centered header on car tabs

--- SoftServeCarMobile/src/activity/MyProfile/MyProfileActivity/CarTabs/CarTabs.tsx
+++ SoftServeCarMobile/src/activity/MyProfile/MyProfileActivity/CarTabs/CarTabs.tsx
@@ -1,8 +1,9 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react'
+import React from 'react';
 import { View } from 'react-native';
 import AddCars from '../../../../components/Cars/AddCars/AddCars';
 import Cars from '../../../../components/Cars/Cars';
+import EditCars from '../../../../components/Cars/EditCars/EditCars';
 
 const StackTabs = createStackNavigator();
 
@@ -10,8 +11,35 @@ export default function CarTabs() {
     return (
         <View style={{ flex: 1, alignSelf: 'stretch' }}>
             <StackTabs.Navigator>
-                <StackTabs.Screen name="Cars" component={Cars}></StackTabs.Screen>
-                <StackTabs.Screen name="AddCars" options={{headerBackAccessibilityLabel: "Add a Car"}} component={AddCars}></StackTabs.Screen>
+                <StackTabs.Screen
+                    name="Cars"
+                    component={Cars}
+                    options={{
+                        title: 'Your Cars',
+                        headerTitleAlign: 'center',
+                        headerTitleStyle: {
+                            textAlign: 'center'
+                        }
+                    }}></StackTabs.Screen>
+                <StackTabs.Screen name="AddCars"
+                    component={AddCars}
+                    options={{
+                        title: 'Add a Car',
+                        headerTitleAlign: 'center',
+                        headerTitleStyle: {
+                            textAlign: 'center'
+                        }
+                    }}></StackTabs.Screen>
+                <StackTabs.Screen
+                    name="EditCars"
+                    component={EditCars}
+                    options={{
+                        title: 'Edit a Car',
+                        headerTitleAlign: 'center',
+                        headerTitleStyle: {
+                            textAlign: 'center'
+                        }
+                    }}></StackTabs.Screen>
             </StackTabs.Navigator>
         </View>
     )
