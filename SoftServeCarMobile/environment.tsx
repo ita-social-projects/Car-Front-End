fd8b1aa527579df7126afad48c0f2e3cb8a4b7a9
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2021-01-25 18:02:37 +0200

    Initial commit

--- SoftServeCarMobile/environment.tsx
+++ SoftServeCarMobile/environment.tsx
@@ -1,5 +1,6 @@
 export const environment = {
-    url: 'http://10.0.2.2:61658/'
+    //url: 'https://car-project.azurewebsites.net/'
+    url: 'http://10.0.2.2:5000/'
 }
 export const routes = {
     apiUrl: environment.url + 'api/'
