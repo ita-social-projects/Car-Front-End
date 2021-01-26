fbf0008f3e081be4b0c258866027d89529e4548b
TetianaLysak <lysak.tetiana22@gmail.com>
2021-01-26 13:27:23 +0200

    Getting data from db

--- SoftServeCarMobile/environment.tsx
+++ SoftServeCarMobile/environment.tsx
@@ -1,5 +1,6 @@
 export const environment = {
     url: 'https://car-project.azurewebsites.net/'
+    //url: 'https://localhost:44346/'
 }
 export const routes = {
     apiUrl: environment.url + 'api/'
--- SoftServeCarMobile/src/activity/journey/journey-activity/segment-control-activities/JourneyPage.tsx
+++ SoftServeCarMobile/src/activity/journey/journey-activity/segment-control-activities/JourneyPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
 import {
   Dimensions,
   Image,
@@ -6,10 +6,22 @@ import {
   Text,
   TouchableOpacity,
   View,
-} from 'react-native';
-import BottomPopup from '../../../../components/bottom-popup/BottomPopup';
+} from "react-native";
+import BottomPopup from "../../../../components/bottom-popup/BottomPopup";
+import { container } from "tsyringe";
+import { AuthContext } from "../../../auth/AuthProvider";
+import JourneyService from "../../../../../api-service/journeyService/JourneyService";
+import { Journey } from "../../../../../models/Journey";
 
 const JourneyPage = () => {
+  const journeyService = container.resolve(JourneyService);
+  const { user } = useContext(AuthContext);
+  const [currentJourney, setJourney] = useState({} as Journey);
+  journeyService
+    .getJourney(1)
+    .then((res) => setJourney(res.data))
+    .catch((e) => console.log(e));
+
   const content = () => {
     return (
       <View style={styles.contentView}>
@@ -17,7 +29,7 @@ const JourneyPage = () => {
           <View style={styles.organizerImageBlock}>
             <Image
               style={styles.organizerImage}
-              source={require('../../../../../images/default-user-photo.jpg')}
+              source={require("../../../../../assets/images/default-user-photo.jpg")}
             />
           </View>
           <View style={styles.organizerInfoBlock}>
@@ -26,7 +38,8 @@ const JourneyPage = () => {
             </Text>
             <View style={styles.organizerSecondaryInfoBlock}>
               <Text style={styles.organizerRoleText}>Experience Designer</Text>
-              <Text style={styles.dateText}>Today at 19:15</Text>
+              <Text style={styles.dateText}>Today at 14:15</Text>
+              <Text>{currentJourney?.id}</Text>
             </View>
           </View>
         </TouchableOpacity>
@@ -37,20 +50,21 @@ const JourneyPage = () => {
   return (
     <>
       <BottomPopup
-        style={{backgroundColor: 'white'}}
+        style={{ backgroundColor: "white" }}
         snapPoints={[
-          '50%',
-          '45%',
-          '40%',
-          '35%',
-          '30%',
-          '25%',
-          '20%',
-          '15%',
-          '10%',
+          "50%",
+          "45%",
+          "40%",
+          "35%",
+          "30%",
+          "25%",
+          "20%",
+          "15%",
+          "10%",
         ]}
         renderContent={content}
-        initialSnap={0}></BottomPopup>
+        initialSnap={0}
+      ></BottomPopup>
     </>
   );
 };
@@ -59,18 +73,18 @@ export default JourneyPage;
 
 const styles = StyleSheet.create({
   contentView: {
-    backgroundColor: 'white',
-    height: Dimensions.get('window').height / 2,
-    alignItems: 'center',
+    backgroundColor: "white",
+    height: Dimensions.get("window").height / 2,
+    alignItems: "center",
   },
   organizerBlock: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '90%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "90%",
   },
   organizerImageBlock: {
     flex: 1,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
   },
   organizerImage: {
     width: 40,
@@ -78,29 +92,29 @@ const styles = StyleSheet.create({
     borderRadius: 100,
   },
   organizerInfoBlock: {
-    flexDirection: 'column',
-    justifyContent: 'space-around',
+    flexDirection: "column",
+    justifyContent: "space-around",
     flex: 5,
   },
   organizerNameText: {
     fontSize: 15,
-    fontWeight: '700',
-    fontFamily: 'Open-Sans-Regular'
+    fontWeight: "700",
+    fontFamily: "Open-Sans-Regular",
   },
   organizerSecondaryInfoBlock: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   organizerRoleText: {
     fontSize: 13,
-    color: '#909095', 
-    fontWeight: '100',
-    fontFamily: 'Open-Sans-Regular'
+    color: "#909095",
+    fontWeight: "100",
+    fontFamily: "Open-Sans-Regular",
   },
   dateText: {
     fontSize: 13,
-    color: '#02A2CF',
-    fontWeight: '700',
-    fontFamily: 'Open-Sans-Regular'
+    color: "#02A2CF",
+    fontWeight: "700",
+    fontFamily: "Open-Sans-Regular",
   },
 });
