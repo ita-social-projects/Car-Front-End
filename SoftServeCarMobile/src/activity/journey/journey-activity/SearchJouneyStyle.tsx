909dc82d6285d2a2112b20a05bcf7df6b79e992a
NazariyChetvertukha <carrrson2207@gmail.com>
2021-01-27 23:44:59 +0200

    Add components for first page of Search Journey (#81)
    * Created and styled components for the first page of Search, wrote a simple test
    
    * Removed code smell

---
+++ SoftServeCarMobile/src/activity/journey/journey-activity/SearchJouneyStyle.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const SearchJouneyStyle = StyleSheet.create({
+    container: {
+        backgroundColor:'#F2F2F2'
+    },
+    recentJourneyText:{
+    //    borderTopColor: '#C1C1C5',
+     //   borderTopWidth:1,
+        paddingBottom: 5,
+        paddingLeft: 10,
+        paddingTop: 8,
+        paddingRight: 10,
+        marginRight:10,
+        fontWeight: 'bold',
+        fontSize:12
+    },
+    topInputContainer:{
+        borderBottomColor: '#C1C1C5',
+        borderBottomWidth:1,
+    }
+});
---
+++ SoftServeCarMobile/src/activity/journey/journey-activity/segment-control-activities/TouchableMapBarStyle.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+const TouchableMapBarStyle = StyleSheet.create({
+    container: {
+        flexDirection:'row',
+        borderWidth: 2,
+        padding: 10,
+        marginRight:20,
+        marginLeft:20,
+        backgroundColor: 'white',
+        fontFamily:'OpenSans'
+    },
+
+    insideText: {
+        flex:1,
+        color: '#909095',
+        marginLeft: 5,
+        fontSize: 16,
+    },
+
+    directionText: {
+        flex:5,
+        color: 'black',
+        marginLeft: 5,
+        fontSize: 16,
+    },
+
+    textInputStyle: {
+        paddingVertical: 8,
+        fontSize: 16,
+        lineHeight: 24,
+        zIndex: 0,
+    },
+
+    barIcon: {
+        flex:1,
+        borderColor: '#EEEEEE',
+    }
+
+});
+export default TouchableMapBarStyle;
