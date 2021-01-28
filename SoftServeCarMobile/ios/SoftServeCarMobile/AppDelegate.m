0eddd3028ea2c0423620f1e2ffde30d954f7feb2
Roman Danylevych <reverend@Romans-MacBook-Pro.local>
2021-01-28 13:20:00 +0200

    Removed Proxima Nova Reference

--- SoftServeCarMobile/ios/SoftServeCarMobile/AppDelegate.m
+++ SoftServeCarMobile/ios/SoftServeCarMobile/AppDelegate.m
@@ -48,6 +48,14 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
+  for (NSString* family in [UIFont familyNames])
+  {
+    NSLog(@"%@", family);
+    for (NSString* name in [UIFont fontNamesForFamilyName: family])
+    {
+      NSLog(@" %@", name);
+    }
+  }
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
