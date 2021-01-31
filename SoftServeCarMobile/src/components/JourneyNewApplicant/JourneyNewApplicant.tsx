12074ef4ef9b00d7156845b565ba2b5b9d6fe3b7
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2021-01-31 14:32:07 +0200

    Markup Commit

--- SoftServeCarMobile/src/components/JourneyNewApplicant/JourneyNewApplicant.tsx
+++ SoftServeCarMobile/src/components/JourneyNewApplicant/JourneyNewApplicant.tsx
@@ -1,19 +1,51 @@
 import React from 'react'
 import {View, Text, Button, Image, Platform} from 'react-native'
-import {style} from './JourneyNewApplicantStyle'
-
+import {style, item} from './JourneyNewApplicantStyle'
 
 export const JourneyNewApplicant = () => {
     return (
         <View style={style.container}>
             <View style={style.row}>
-                <View style={style.item50}>
+                <View style={item(50)}>
                     <Text style={style.header}>New Applicant</Text>
                 </View>
-                <View style={style.item50}>
+                <View style={item(50)}>
                     <Text style={style.snooze}>Snooze</Text>
                 </View>
             </View>
+            <View style={[style.row, style.title]}>
+
+                <View style={item(20)}>
+                    <View style={style.circle}>
+                        <Text style={style.icon}>JB</Text>
+                    </View>
+                </View>
+                <View style={item(70)}>
+                    <View style={style.profile}>
+                        <Text style={style.name}>Jaylon Ekstrom Bothman</Text>
+                        <Text style={style.bio}>Experience Design Intermediate</Text>
+                        <Text style={style.achievements}>123 rides, 2 badges</Text>
+                    </View>
+                </View>
+                <View style={item(10)}>
+                    <View>
+                        <Text style={style.more}>...</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={[style.row, style.commentsBox]}>
+                <Text style={style.commentsText}>Hey! Do you mind if I throw my suitcase in the trunk?</Text>
+                <View style={style.commentsBoxAfter}/>
+            </View>
+            <View style={[style.row, style.options]}>
+                <Text style={style.optionsHeader}>I’m Traveling with a baggage.</Text>
+                <Text style={style.optionsValue}>The baggage is allowed in your car</Text>
+                <View style={style.optionsLine}/>
+            </View>
+            <View style={[style.row, style.stops]}>
+                <Text style={style.optionsHeader}>Jaylon’s stop in your Journey</Text>
+            </View>
+
 
         </View>
     )
--- SoftServeCarMobile/src/components/JourneyNewApplicant/JourneyNewApplicantStyle.ts
+++ SoftServeCarMobile/src/components/JourneyNewApplicant/JourneyNewApplicantStyle.ts
@@ -1,7 +1,13 @@
 import {Platform, StyleSheet, View} from "react-native";
-import {ProximaNova} from "../../../font-manager";
+import {OpenSans, ProximaNova} from "../../../font-manager";
+import EStyleSheet from "react-native-extended-stylesheet";
 
-export const style = StyleSheet.create({
+export let item = function (percent: number) {
+    return {
+        width: percent + '%'
+    }
+}
+export const style = EStyleSheet.create({
     container: {
         backgroundColor: "#FFFFFF"
         , borderWidth: 1
@@ -27,37 +33,172 @@ export const style = StyleSheet.create({
 
         , elevation: 10
     }
+    , circle: {
+        backgroundColor: "#A5C500"
+        , borderRadius: 90
+        , height: 57
+        , width: 57
+        , display: "flex"
+        , alignItems: "center"
+        , justifyContent: 'center'
+        , marginTop: 5
+    }
     , row: {
-        flex: 1
+        flex: 0
         , flexDirection: 'row'
         , flexWrap: 'wrap'
         , alignItems: 'flex-start'
-    }
-    , item50: {
-        width: '50%'
-    }
 
+    }
+    , title: {
+        marginTop: 32
+    }
     , header: {
-        fontFamily: ProximaNova.Bold,
-        fontSize: 14,
-        lineHeight: 16,
-        display: "flex",
-        alignItems: "center",
-        letterSpacing: 0.2,
-        textTransform: "uppercase",
-        color: "#000000"
+        fontFamily: ProximaNova.Bold
+        , fontSize: '1rem'
+        , lineHeight: 16
+        , display: "flex"
+        , alignItems: "center"
+        , letterSpacing: 0.2
+        , textTransform: "uppercase"
+        , color: "#000000"
     }
     , snooze: {
-        fontFamily: ProximaNova.Bold,
-        fontSize: 14,
-        lineHeight: 16,
-        display: "flex",
-        alignItems: "center",
-        textAlign:'right',
-        letterSpacing: 0.875,
-        textTransform: "capitalize",
-        color: "#02A2CF"
+        fontFamily: ProximaNova.Bold
+        , fontSize: '1rem'
+        , lineHeight: 16
+        , display: "flex"
+        , alignItems: "center"
+        , textAlign: 'right'
+        , letterSpacing: 0.875
+        , textTransform: "capitalize"
+        , color: "#02A2CF"
     }
+    , ellipse: {
+        width: 100,
+        height: 100
+    }
+    , icon: {
+        fontFamily: OpenSans.Bold
+        , fontSize: '0.9375rem'
+        , lineHeight: 16
+        , display: "flex"
+        , alignItems: "center"
+        , textAlign: 'center'
+        , textTransform: "uppercase"
+        , color: '#ffffff'
+    }
+    , profile: {
+        //alignItems: "center"
+         textAlign: 'left'
+        , marginLeft: 10
+    }
+    , name: {
+        fontFamily: ProximaNova.Bold
+        , fontSize: '1.1rem'
+        , lineHeight: 21
+        , color: '#000000'
+    }
+    , bio: {
+        fontFamily: ProximaNova.Regular
+        , fontSize: '0.85rem'
+        , lineHeight: 14
+        , color: '#000000'
+        , opacity: 0.5
+        //, mixBlendMode: 'normal'
+        , marginTop: '0.7rem'
+    }
+    , achievements: {
+        fontFamily: ProximaNova.Regular
+        , fontSize: 15
+        , lineHeight: 14
+        , color: '#000000'
+        , opacity: 0.5
+        //, mixBlendMode: 'normal'
+        , marginTop: '0.7rem'
+    }
+    , more: {
+        fontFamily: OpenSans.ExtraBold
+        , fontSize: 20
+        //, fontWeight: '900'
+        , letterSpacing: 3
+        //, lineHeight: 50
+        , marginTop: -10
+        , paddingTop: 0
+        , color: '#000000'
+    }
+    , commentsBox: {
+        borderWidth: 1
+        , borderColor: 'rgba(151, 151, 151, 0.3)'
+        , borderStyle: "solid"
+        , backgroundColor: '#ffffff'
+        , marginTop: '1.5rem'
+    }
+    , commentsBoxAfter:{
+        position: 'absolute'
+        , width: '1rem'
+        , height: '1rem'
+        , borderWidth:1
+        , borderStyle: 'solid'
+        //, borderColor: '#000000'
+        //, borderTopColor: '#ffffff'
+        , borderTopColor: 'rgba(0,0,0,0)'
+        , borderLeftColor: 'rgba(0,0,0,0)'
+        , borderRightColor: 'rgba(151, 151, 151, 0.3)'
+        , borderBottomColor: 'rgba(151, 151, 151, 0.3)'
+
+        , top: '-0.5rem'
+        , left: '50%'
+        , marginLeft: '-1rem'
+        , transform: [
+            {rotateZ: '-135deg'}
+        ]
+        //, marginTop: '-2.8rem'
+        , backgroundColor: '#ffffff'
+        , opacity: 1
+    }
+    , commentsText:{
+        margin: '1.5rem'
+        , textAlign: 'center'
+        , fontFamily: ProximaNova.Regular
+        , fontSize: '1.2rem'
+        , paddingLeft: '1rem'
+        , paddingRight: '1rem'
+    }
+    , options:{
+        marginTop: '1.5rem'
+    }
+    , optionsHeader:{
+        fontFamily: OpenSans.Bold
+        , lineHeight: '1.1rem'
+        , fontSize: '1rem'
+    }
+    , optionsValue:{
+        fontFamily: OpenSans.Regular
+        , lineHeight: '1.2rem'
+        , fontSize: '1rem'
+        , marginTop: '0.5rem'
+    }
+    , optionsLine:{
+        minHeight:10
+        , minWidth:'99%'
+        , borderWidth: 1
+        , borderTopColor: 'rgba(0,0,0,0)'
+        , borderLeftColor: 'rgba(0,0,0,0)'
+        , borderRightColor: 'rgba(0,0,0,0)'
+        , borderBottomColor: '#C1C1C5'
+        , borderStyle: "solid"
+        , backgroundColor: '#ffffff'
+        , marginLeft: 'auto'
+        , marginRight: 'auto'
+    }
+    , stopsHeader:{
+        fontFamily: OpenSans.Bold
+        , lineHeight: '1.1rem'
+        , fontSize: '1rem'
+    }
+
+
 
 });
 
