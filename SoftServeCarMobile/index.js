85f3b224755cfe35125396867014813e161fae48
Roman Danylevych <roma.dan2001@gmail.com>
2021-02-02 12:40:01 +0200

    Resolved conflicts in Routes

--- SoftServeCarMobile/index.js
+++ SoftServeCarMobile/index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { AppRegistry } from 'react-native';
-=======
-import {AppRegistry} from 'react-native';
->>>>>>> develop
 import App from './App';
 import { name as appName } from './app.json';
 
