b95073b9cc242186c181c9fb6feebb2eccc107dc
dreamofmaks <dreamofmaks@ukr.net>
2021-02-04 16:19:03 +0200

    remove unusefull files

--- SoftServeCarMobile/__tests__/Chat-test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Chat from '../src/activity/chat/Chat';
-
-test('renders correctly', () => {
-    renderer.create(<Chat />);
-})
--- SoftServeCarMobile/__tests__/SimpleMessage-test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import SimpleMessage from '../src/activity/chat/SimpleMessage'
-
-test('renders correctly', () => {
-    renderer.create(<SimpleMessage />);
-});
