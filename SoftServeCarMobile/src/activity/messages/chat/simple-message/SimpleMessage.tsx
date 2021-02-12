b8fb1652858bf39a5bc266341e7e6dbfc0387818
Roman Alberda <58659160+RamzesUAA@users.noreply.github.com>
2021-02-12 12:54:34 +0200

    Added filter item (#144)
    * added filter field into chat
    
    * Update Environment.tsx
    
    * Update Environment.tsx
    
    * Update MessagesTabs.tsx
    
    * Update MessagesTabs.tsx
    
    * added changes according to review

--- SoftServeCarMobile/src/activity/messages/chat/simple-message/SimpleMessage.tsx
+++ SoftServeCarMobile/src/activity/messages/chat/simple-message/SimpleMessage.tsx
@@ -1,94 +1,95 @@
-import React, { useContext, useEffect, useState } from "react";
-import {
-    ActivityIndicator,
-    Button,
-    FlatList,
-    Image,
-    Text,
-    TouchableOpacity,
-    View
-} from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import "reflect-metadata";
-import { container } from "tsyringe";
 import ChatService from "../../../../../api-service/chat-service/ChatService";
 import { AuthContext } from "../../../auth/AuthProvider";
+import React, { useEffect, useState, useContext } from 'react';
+import { SafeAreaView, Text, View, TouchableOpacity, Image, FlatList } from 'react-native';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import "reflect-metadata";
+import { container } from 'tsyringe';
+import { SearchBar } from "react-native-elements";
 import SimpleMessageStyle from "./SimpleMessageStyle";
-import * as navigation from "../../../../components/navigation/Navigation";
 
-const SimpleMessage = (props: {
-    navigation: { navigate: (arg0: string) => void };
-}) => {
+
+const SimpleMessage = (props: any) => {
     const chatService = container.resolve(ChatService);
-    const [data, setData] = useState([]);
-    const [isLoading, setLoading] = useState(true);
+    const [filteredDataSource, setFilteredDataSource] = useState([]);
+    const [masterDataSource, setMasterDataSource] = useState([]);
+    const [search, setSearch] = useState('');
     const { user } = useContext(AuthContext);
 
     useEffect(() => {
-        chatService.getChat(user?.id).then((res) => {
-            const chats = res.data;
-            console.log(chats);
-            setData(chats);
-            setLoading(false);
-        });
+        chatService.getChat(user?.id)
+            .then(res => {
+                const chats = res.data;
+                console.log(chats);
+                setFilteredDataSource(chats);
+                setMasterDataSource(chats);
+            })
     }, []);
 
-    if (isLoading) {
-        return (
-            <View>
-                <ActivityIndicator size="large" color="#5500dc" />
-            </View>
-        );
-    }
+    const setSearchFilter = (text) => {
+        if (text) {
+            const newData = masterDataSource.filter(function (item) {
+                const itemData = item.name
+                    ? item.name.toUpperCase()
+                    : ''.toUpperCase();
+                const textData = text.toUpperCase();
+                return itemData.indexOf(textData) > -1;
+            });
+            setFilteredDataSource(newData);
+            setSearch(text);
+        } else {
+            setFilteredDataSource(masterDataSource);
+            setSearch(text);
+        }
+    };
 
-    return (
-        <View style={SimpleMessageStyle.container}>
-            <Button
-                title="Chat"
-                color="#000000"
-                onPress={() => {
-                    navigation.navigate("Chat", {});
-                }}
-            />
-            <FlatList
-                data={data}
-                keyExtractor={({ id }) => "" + id}
-                renderItem={({ item }) => (
-                    <View style={SimpleMessageStyle.main}>
-                        <View style={SimpleMessageStyle.button}>
-                            <View style={{ flexDirection: "row" }}>
-                                <Image
-                                    style={SimpleMessageStyle.image}
-                                    source={{
-                                        uri:
-                                            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
-                                    }}
-                                />
-                                <View>
-                                    <Text style={SimpleMessageStyle.fonts}>
-                                        {item.name}
-                                    </Text>
-                                    <Text style={SimpleMessageStyle.text}>
-                                        {item.name}{" "}
-                                    </Text>
-                                </View>
-                            </View>
-                            <TouchableOpacity
-                                style={{ paddingTop: 12 }}
-                                onPress={() =>
-                                    props.navigation.navigate("Chat")
-                                }
-                            >
-                                <View>
-                                    <Ionicons name={"chatbubbles"} size={20} />
-                                </View>
-                            </TouchableOpacity>
+    const renderHeader = () => {
+        return (props.isOpenFilter) ? (
+            <SearchBar
+                searchIcon={{ color: "black", size: 28 }}
+                onChangeText={(text) => setSearchFilter(text)}
+                onClear={() => setSearchFilter('')}
+                placeholder={'Search in Messages'}
+                value={search}
+                containerStyle={SimpleMessageStyle.containerStyle}
+                inputContainerStyle={SimpleMessageStyle.inputContainerStyle}
+            />) : <View />
+    };
+
+    const ItemView = ({ item }) => {
+        return (
+            <View style={SimpleMessageStyle.main}>
+                <View style={SimpleMessageStyle.button}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Image style={SimpleMessageStyle.image}
+                            source={{ uri: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg' }} />
+                        <View>
+                            <Text style={SimpleMessageStyle.fonts}>{item.name}</Text>
+                            <Text style={SimpleMessageStyle.textStyle}>{item.name} </Text>
                         </View>
                     </View>
-                )}
-            />
-        </View>
+                    <TouchableOpacity style={{ paddingTop: 12, }}
+                        onPress={() => props.navigation.navigate("Chat")}>
+                        <View><Ionicons name={'chatbubbles'} size={20} /></View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={SimpleMessageStyle.container}>
+                {renderHeader()}
+                <FlatList
+                    data={filteredDataSource}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={ItemView}
+                />
+            </View>
+        </SafeAreaView>
     );
-};
+}
 
 export default SimpleMessage;
+
