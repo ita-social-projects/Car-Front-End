0f867b28981aad1b1d4effe8fef8db186467b43e
RomanKovalchuk <mrbylba@gmail.com>
2021-02-14 23:35:38 +0200

    delete node modules

--- SoftServeCarMobile/node_modules/react-native-app-auth/ios/RNAppAuthAuthorizationFlowManager.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "RNAppAuthAuthorizationFlowManagerDelegate.h"
-
-@protocol RNAppAuthAuthorizationFlowManager <NSObject>
-@required
-@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
-@end
--- SoftServeCarMobile/node_modules/react-native-modal-dropdown/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by sohobloo on 16/9/14.
- */
-
-import ModalDropdown from './components/ModalDropdown';
-export default ModalDropdown;
-module.exports = ModalDropdown;
