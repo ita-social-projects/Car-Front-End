1b2b3c0c82da849d134b8bcdec2e22266430edba
Roman Danylevych <roma.dan2001@gmail.com>
2021-02-14 21:54:23 +0200

    Added node modules

---
+++ SoftServeCarMobile/node_modules/jest-mock-axios/dist/lib/index.d.ts
@@ -0,0 +1,2 @@
+import MockAxios from "./mock-axios";
+export default MockAxios;
---
+++ SoftServeCarMobile/node_modules/jest-mock-axios/dist/lib/mock-axios.d.ts
@@ -0,0 +1,10 @@
+/**
+ * TypeScript version of Axios mock for unit testing with [Jest](https://facebook.github.io/jest/).
+ * This file is based on https://gist.github.com/tux4/36006a1859323f779ab0
+ *
+ * @author   knee-cola <nikola.derezic@gmail.com>
+ * @license  @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+import { AxiosMockType } from "./mock-axios-types";
+declare const MockAxios: AxiosMockType;
+export default MockAxios;
