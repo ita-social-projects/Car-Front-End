853d4f11aac51c2aa087d458b2b9ad8d18c976eb
Roman Danylevych <reverend@Romans-MacBook-Pro.local>
2021-02-15 10:53:18 +0200

    Removed node_modules

---
+++ SoftServeCarMobile/ios/Pods/Headers/Public/RNTextGradientView/RNTextGradientUtils.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-text-gradient/ios/RNTextGradientUtils.h
--- SoftServeCarMobile/node_modules/react-native-app-auth/ios/RNAppAuth.h
+++
@@ -1,6 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RNAppAuth : NSObject <RCTBridgeModule>
-
-@end
-  
