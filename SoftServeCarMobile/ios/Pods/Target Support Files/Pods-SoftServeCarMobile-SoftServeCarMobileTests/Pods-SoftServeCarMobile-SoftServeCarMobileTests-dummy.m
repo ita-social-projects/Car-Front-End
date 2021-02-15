c60bc36140297db2e58b8d668b7340e6161d36e7
Roman Danylevych <reverend@Romans-MacBook-Pro.local>
2021-02-15 14:25:54 +0200

    Renamed project

--- SoftServeCarMobile/ios/Pods/Target Support Files/Pods-SoftServeCarMobile-SoftServeCarMobileTests/Pods-SoftServeCarMobile-SoftServeCarMobileTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SoftServeCarMobile_SoftServeCarMobileTests : NSObject
-@end
-@implementation PodsDummy_Pods_SoftServeCarMobile_SoftServeCarMobileTests
-@end
