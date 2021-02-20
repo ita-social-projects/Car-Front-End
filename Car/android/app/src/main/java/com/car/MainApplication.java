9c412fdd43c21bb2d74796e85ca4f80398d027fa
Roman Danylevych <roma.dan2001@gmail.com>
2021-02-20 21:04:17 +0200

    Removed timers

--- Car/android/app/src/main/java/com/car/MainApplication.java
+++ Car/android/app/src/main/java/com/car/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.ocetnik.timer.BackgroundTimerPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import iyegoroff.RNTextGradient.RNTextGradientPackage;
 import com.imagepicker.ImagePickerPackage;
