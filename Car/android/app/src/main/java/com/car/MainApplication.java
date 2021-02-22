f95ff831db158c9dbee424da3a46b0d52f56637a
Roman Danylevych <roma.dan2001@gmail.com>
2021-02-22 18:50:43 +0200

    Fixed my profile header

--- Car/android/app/src/main/java/com/car/MainApplication.java
+++ Car/android/app/src/main/java/com/car/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativerestart.RestartPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import iyegoroff.RNTextGradient.RNTextGradientPackage;
 import com.imagepicker.ImagePickerPackage;
