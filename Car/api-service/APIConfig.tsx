a8c790f27d259bd14eb548f01b33adb3e0f331b6
Roman Alberda <58659160+RamzesUAA@users.noreply.github.com>
2021-02-26 01:29:02 +0200

    Update APIConfig.tsx

--- Car/api-service/APIConfig.tsx
+++ Car/api-service/APIConfig.tsx
@@ -1,6 +1,5 @@
 const APIConfig = {
-    // URL: "https://car-project.azurewebsites.net/"
-    URL: "http://10.0.2.2:61658/"
+    URL: "https://car-project.azurewebsites.net/"
 };
 
 export default APIConfig;
