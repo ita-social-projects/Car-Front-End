aa2603857be4a6456b9671dd6ae62bfacd1c4ada
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2021-02-28 17:11:16 +0200

    Update APIConfig.tsx

--- Car/api-service/APIConfig.tsx
+++ Car/api-service/APIConfig.tsx
@@ -1,6 +1,5 @@
 const APIConfig = {
-    //URL: "https://car-project.azurewebsites.net/"
-    URL: "http://192.168.0.105:5000/"
+    URL: "https://car-project.azurewebsites.net/"
 };
 
 export default APIConfig;
