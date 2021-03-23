65f74832153d77e8cfdff4bdd5d2aabb258d4b9a
vovazavislyak <vowa222216@gmail.com>
2021-03-23 20:32:55 +0200

    Created CarDropDownPickerProps interface

---
+++ Car/src/components/car-drop-down-picker/CarDropDownPickerProps.ts
@@ -0,0 +1,18 @@
+import CarDropDownPickerItem from "./CarDropDownItem";
+
+interface CarDropDownPickerProps {
+    placeHolder?: string,
+    items?: CarDropDownPickerItem[] | null,
+    zIndex?: number,
+    required?: boolean,
+    disabled?: boolean,
+    defaultValue?: null,
+    // eslint-disable-next-line unused-imports/no-unused-vars
+    selectHandle?: (item: CarDropDownPickerItem) => void,
+    onOpen?: () => void,
+    // eslint-disable-next-line unused-imports/no-unused-vars
+    controller?: (instance: any) => void,
+    style?: object
+}
+
+export default CarDropDownPickerProps;
