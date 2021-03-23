12333e86306f3feede5547a4cfb857b97e5aac7c
vovazavislyak <vowa222216@gmail.com>
2021-03-23 17:40:11 +0200

    Created IndicatorProps interface

---
+++ Car/src/components/activity-indicator/IndicatorProps.ts
@@ -0,0 +1,9 @@
+import { ColorValue } from "react-native";
+
+interface IndicatorProps {
+    size?: number | "small" | "large";
+    color?: ColorValue;
+    text?: string;
+}
+
+export default IndicatorProps;
