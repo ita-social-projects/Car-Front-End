c8c4d7b36731ce41d73d994a076dda7ab05feca6
vovazavislyak <vowa222216@gmail.com>
2021-04-21 11:35:12 +0300

    Restyled SeatsInputSpinner

--- Car/src/activity/journey/journey-activity/input-spinner/SeatsInputSpinnerStyle.tsx
+++ Car/src/activity/journey/journey-activity/input-spinner/SeatsInputSpinnerStyle.tsx
@@ -4,6 +4,7 @@ export const SeatsInputSpinnerStyle = StyleSheet.create({
     container: {
         flexDirection: "row",
         justifyContent: "space-between",
+        alignItems: "center",
         marginRight: 20,
         marginTop: 16
     },
@@ -11,12 +12,17 @@ export const SeatsInputSpinnerStyle = StyleSheet.create({
     spinnerContainer: {
         borderColor: "#000000",
         borderWidth: 2,
-        width: "100%",
+        width: 145,
         borderRadius: 0
     },
 
     descriptionText: {
         marginLeft: 22,
+        fontWeight: "bold"
+    },
+
+    input: {
+        borderLeftWidth: 2,
         fontWeight: "bold"
     }
 });
