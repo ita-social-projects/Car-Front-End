c1a29cff40c66ad87f2a09f8dc5b087d5558f349
Yurii Danyliv <yura.dng@gmail.com>
2021-05-30 21:01:07 +0300

    added internet connection check

--- Car/src/components/navigation/Providers.tsx
+++ Car/src/components/navigation/Providers.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AuthProvider from "../auth/AuthProvider";
+import ErrorAlert from "../error-alert/ErrorAlert";
 import Routes from "./Routes";
+import NetInfo from "@react-native-community/netinfo";
 
-const Providers = () => (
-    <AuthProvider>
-        <Routes />
-    </AuthProvider>
-);
+const Providers = () => {
+    useEffect(() => {
+        const unsubscribe = NetInfo.addEventListener(state => {
+            if (!state.isInternetReachable) {
+                ErrorAlert("No internet connection");
+            }
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    });
+
+    return (
+        <AuthProvider>
+            <Routes />
+        </AuthProvider>
+    );
+};
 
 export default Providers;
