36fafbd1fab22154a654943e9db2c32c2695e61a
Ilya Shevchyk <il2001sh@gmail.com>
2021-06-05 11:15:37 +0300

    removed unused stuff

--- Car/android/app/src/main/java/com/car/MainApplication.kt
+++ Car/android/app/src/main/java/com/car/MainApplication.kt
@@ -2,19 +2,16 @@ package com.car;
 
 import android.app.*
 import android.content.Context
-import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import android.widget.RemoteViews
-import com.car.Entities.User
-import com.car.NotificationModule.NotificationPackage
 import com.car.SoftKeysModule.SoftKeysPackage
 import com.facebook.react.*
 import com.facebook.soloader.SoLoader
-import com.microsoft.signalr.HubConnection
 import java.lang.reflect.InvocationTargetException
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -31,8 +28,8 @@ class MainApplication : Application(), ReactApplication {
 
         override fun getPackages(): List<ReactPackage> {
             var packages = PackageList(this).packages
-            packages.add(NotificationPackage())
             packages.add(SoftKeysPackage())
+            //packages.add(ReactNativePushNotificationPackage())
             return packages
         }
 
@@ -49,11 +46,6 @@ class MainApplication : Application(), ReactApplication {
         super.onCreate()
         SoLoader.init(this,  /* native exopackage */false)
         initializeFlipper(this, reactNativeHost.reactInstanceManager)
-
-        val intent = Intent(this, NotificationService::class.java)
-        startService(intent)
-
-        //sendNotification(1, "Roman Danylevych's ride", "Implementation of notifications is in progress!")
     }
 
     companion object {
@@ -87,46 +79,5 @@ class MainApplication : Application(), ReactApplication {
                 }
             }
         }
-    }
-
-    fun sendNotification(id: Int, title: String, text: String) {
-        val intent = Intent(this, LauncherActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        var notificationColor = Color.BLACK
-
-        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-            Configuration.UI_MODE_NIGHT_YES -> notificationColor = Color.WHITE
-            Configuration.UI_MODE_NIGHT_NO -> notificationColor = Color.BLACK
-        }
-
-        val contentView = RemoteViews(packageName, R.layout.notification_layout).also {
-            it.setTextViewText(R.id.tv_title, title)
-            it.setTextViewText(R.id.tv_content, text)
-            it.setTextColor(R.id.tv_content, notificationColor)
-            it.setTextColor(R.id.tv_title, notificationColor)
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
-            notificationChannel.enableLights(true)
-            notificationChannel.lightColor = Color.CYAN
-            notificationChannel.enableVibration(true)
-            notificationManager.createNotificationChannel(notificationChannel)
-            builder = Notification.Builder(this, channelId)
-                    .setContent(contentView)
-                    .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher_round))
-                    .setContentIntent(pendingIntent)
-        } else {
-            builder = Notification.Builder(this)
-                    .setContent(contentView)
-                    .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher_round))
-                    .setContentIntent(pendingIntent)
-        }
-
-        notificationManager.notify(id, builder.build())
     }
 }
