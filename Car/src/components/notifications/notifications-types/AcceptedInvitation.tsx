a8ce7452fe8d5cabd91f9a157c7453d815359c4c
Yurii Danyliv <yura.dng@gmail.com>
2021-06-10 08:40:30 +0300

    added stubs to not implemented notifications

---
+++ Car/src/components/notifications/notifications-types/AcceptedInvitation.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import MinimizedNotification from "../../minimized-notification/MinimizedNotification";
+import NotificationHeader from "../notification-header/NotificationHeader";
+import NotificationModalBase from "../notification-modal-base/NotificationModalBase";
+import NotificationProps from "../NotificationProps";
+
+const AcceptedInvitation = (props: NotificationProps) => {
+    const [modalVisible, setModalVisible] = useState(props.visible);
+
+    return (
+        <>
+            <MinimizedNotification
+                notificationId={props.notificationId}
+                user={props.sender}
+                notificationTitle={"Accepted Invitation (Not implemented!)"}
+                read={props.read}
+                date={props.date}
+                openModal={() => setModalVisible(true)}
+            />
+            <NotificationModalBase isVisible={modalVisible!}>
+                <NotificationHeader
+                    title="Not implemented!"
+                    message=""
+                    sender={props.sender}
+                    disableModal={() => setModalVisible(false)}
+                />
+            </NotificationModalBase>
+        </>
+    );
+};
+
+export default AcceptedInvitation;
---
+++ Car/src/components/notifications/notifications-types/HRMarketingSurvey.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import MinimizedNotification from "../../minimized-notification/MinimizedNotification";
+import NotificationHeader from "../notification-header/NotificationHeader";
+import NotificationModalBase from "../notification-modal-base/NotificationModalBase";
+import NotificationProps from "../NotificationProps";
+
+const HRMarketingSurvey = (props: NotificationProps) => {
+    const [modalVisible, setModalVisible] = useState(props.visible);
+
+    return (
+        <>
+            <MinimizedNotification
+                notificationId={props.notificationId}
+                user={props.sender}
+                notificationTitle={"HR Marketing Survey (Not implemented!)"}
+                read={props.read}
+                date={props.date}
+                openModal={() => setModalVisible(true)}
+            />
+            <NotificationModalBase isVisible={modalVisible!}>
+                <NotificationHeader
+                    title="Not implemented!"
+                    message=""
+                    sender={props.sender}
+                    disableModal={() => setModalVisible(false)}
+                />
+            </NotificationModalBase>
+        </>
+    );
+};
+
+export default HRMarketingSurvey;
