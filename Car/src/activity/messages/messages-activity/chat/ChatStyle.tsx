4d3b5b3457968c02df93a26215ffe05bc6219c92
Nazariy <59841390+Nazariy-228@users.noreply.github.com>
2021-08-12 12:42:19 +0300

    Revert "Revert "424 chat header disapears while texting""

--- Car/src/activity/messages/messages-activity/chat/ChatStyle.tsx
+++ Car/src/activity/messages/messages-activity/chat/ChatStyle.tsx
@@ -3,8 +3,6 @@ import { StyleSheet } from "react-native";
 const ChatStyle = StyleSheet.create({
     messageField: {
         flex: 1,
-        width: "100%",
-        height: "100%"
     },
 
     chatWrapper: {
