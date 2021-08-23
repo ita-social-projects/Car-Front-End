cadecbbf7b720a5c85a2bf9134ed7b25f0f04d94
AndriiDzendzia <andriidzendzia@gmail.com>
2021-08-23 21:07:50 +0300

    Closes #644 [Main page] misalignments of font sizes when using 'Large fonts' settings on phone

--- Car/index.js
+++ Car/index.js
@@ -3,8 +3,11 @@ import EStyleSheet from "react-native-extended-stylesheet";
 import App from "./App";
 import { name as appName } from "./app.json";
 import ConfigPushNotifications from "./src/components/notifications/ConfigPushNotifications";
+import ConfigTextScaling from "./src/components/text/ConfigTextScaling";
 
 ConfigPushNotifications();
+
+ConfigTextScaling();
 
 AppRegistry.registerComponent(appName, () => App);
 EStyleSheet.build();
