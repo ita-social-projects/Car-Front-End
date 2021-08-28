ea8b51ff01f0b96d654987a4ed45ee0ab4bd4c8f
Vlas <vlas.kornova.knm.2019@lpnu.ua>
2021-08-28 17:47:40 +0300

    Added ability to choose passengers quantity(with wisual bug)

--- Car/__tests__/activity/journey/SearchJourney-test.tsx
+++ Car/__tests__/activity/journey/SearchJourney-test.tsx
@@ -67,193 +67,38 @@ describe("JourneySearch Test", () => {
             isConfirmed={true}
             setDate={[Function]}
           />
-          <JourneyCreationDropDownPicker
-            isVisible={false}
-            items={
-              Array [
-                Object {
-                  "label": "1",
-                  "value": 1,
-                },
-                Object {
-                  "label": "2",
-                  "value": 2,
-                },
-                Object {
-                  "label": "3",
-                  "value": 3,
-                },
-                Object {
-                  "label": "4",
-                  "value": 4,
-                },
-              ]
-            }
-            onChangeItem={[Function]}
-            onOpen={[Function]}
-            paddingLeft={105}
-            placeholder="Choose a Car:"
-            searchable={true}
-            valueId={1}
-          />
-          <View
-            style={
-              Object {
-                "alignItems": "center",
-                "flexDirection": "row",
-                "fontSize": 13,
-                "lineHeight": 16,
-                "marginLeft": 21,
-                "marginRight": 0,
-                "marginTop": 24,
-              }
-            }
-          >
-            <Text
-              style={
-                Array [
-                  Object {
-                    "flex": 1,
-                    "fontWeight": "bold",
-                  },
-                  Object {
-                    "color": "black",
-                  },
-                ]
-              }
-            >
-              Fee
-            </Text>
-            <View
-              style={
-                Object {
-                  "flexDirection": "row",
-                }
-              }
-            >
-              <ForwardRef
-                onPress={[Function]}
-                style={
+          <View>
+            <ScrollView>
+              <JourneyCreationDropDownPicker
+                isVisible={false}
+                items={
                   Array [
                     Object {
-                      "backgroundColor": "#000000",
-                      "borderWidth": 2,
-                      "flexDirection": "row",
-                      "justifyContent": "flex-end",
-                      "paddingHorizontal": 16,
-                      "paddingVertical": 6,
+                      "label": "1",
+                      "value": 1,
                     },
                     Object {
-                      "backgroundColor": "#000000",
-                      "borderColor": "#000000",
-                      "color": "#FFFFFF",
+                      "label": "2",
+                      "value": 2,
+                    },
+                    Object {
+                      "label": "3",
+                      "value": 3,
+                    },
+                    Object {
+                      "label": "4",
+                      "value": 4,
                     },
                   ]
                 }
-              >
-                <Text
-                  style={
-                    Array [
-                      Object {
-                        "fontSize": 16,
-                        "fontWeight": "bold",
-                        "lineHeight": 18,
-                        "textTransform": "uppercase",
-                      },
-                      Object {
-                        "backgroundColor": "#000000",
-                        "borderColor": "#000000",
-                        "color": "#FFFFFF",
-                      },
-                    ]
-                  }
-                >
-                  All
-                </Text>
-              </ForwardRef>
-              <ForwardRef
-                onPress={[Function]}
-                style={
-                  Array [
-                    Object {
-                      "backgroundColor": "#000000",
-                      "borderWidth": 2,
-                      "flexDirection": "row",
-                      "justifyContent": "flex-end",
-                      "paddingHorizontal": 16,
-                      "paddingVertical": 6,
-                    },
-                    Object {
-                      "backgroundColor": "#FFFFFF",
-                      "borderColor": "#000000",
-                      "color": "#000000",
-                    },
-                  ]
-                }
-              >
-                <Text
-                  style={
-                    Array [
-                      Object {
-                        "fontSize": 16,
-                        "fontWeight": "bold",
-                        "lineHeight": 18,
-                        "textTransform": "uppercase",
-                      },
-                      Object {
-                        "backgroundColor": "#FFFFFF",
-                        "borderColor": "#000000",
-                        "color": "#000000",
-                      },
-                    ]
-                  }
-                >
-                  Free
-                </Text>
-              </ForwardRef>
-              <ForwardRef
-                onPress={[Function]}
-                style={
-                  Array [
-                    Object {
-                      "alignItems": "center",
-                      "backgroundColor": "white",
-                      "borderWidth": 2,
-                      "flexDirection": "row",
-                      "justifyContent": "flex-end",
-                      "marginRight": 20,
-                      "paddingHorizontal": 16,
-                      "paddingVertical": 4,
-                    },
-                    Object {
-                      "backgroundColor": "#FFFFFF",
-                      "borderColor": "#000000",
-                      "color": "#000000",
-                    },
-                  ]
-                }
-              >
-                <Text
-                  style={
-                    Array [
-                      Object {
-                        "fontSize": 16,
-                        "fontWeight": "bold",
-                        "lineHeight": 18,
-                        "textTransform": "uppercase",
-                      },
-                      Object {
-                        "backgroundColor": "#FFFFFF",
-                        "borderColor": "#000000",
-                        "color": "#000000",
-                      },
-                    ]
-                  }
-                >
-                  Paid
-                </Text>
-              </ForwardRef>
-            </View>
+                onChangeItem={[Function]}
+                onOpen={[Function]}
+                paddingLeft={100}
+                placeholder="Passengers:"
+                searchable={true}
+                valueId={1}
+              />
+            </ScrollView>
           </View>
           <View
             style={
