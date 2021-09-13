d888ee2ddcb5615dcd7379036a4f7aea5eadda0a
Vlas2001 <81962606+Vlas2001@users.noreply.github.com>
2021-09-13 06:10:21 +0000

    689 app settings page (#563)
    * Added buttons without pictures
    
    * Added new buttons with icons and hidden them until next release
    
    * Hidden app settings button

--- Car/src/components/choose-opton/ChooseOption.tsx
+++ Car/src/components/choose-opton/ChooseOption.tsx
@@ -4,26 +4,33 @@ import { Switch } from "react-native-gesture-handler";
 import DM from "../styles/DM";
 import ChooseOptionStyle from "./ChooseOptionStyle";
 import ChooseOptionProps from "./ChooseOptionProps";
+import TouchableNavigationCardStyle from "../touchable-navigation-card/TouchableNavigationCardStyle";
 
 const ChooseOption = (props: ChooseOptionProps) => {
     return (
         <View style={ChooseOptionStyle.preferencesContainer}>
+            <View style={TouchableNavigationCardStyle.cardInformationContainer}>
+                <View style={{ flexDirection: "row" }}>
+                    {props.picture != undefined ? (
+                        <View style={TouchableNavigationCardStyle.pictureContainer}>
+                            {props.picture}
+                        </View>
+                    ) : (
+                        <View />
+                    )}
+                </View>
+            </View>
             <View style={ChooseOptionStyle.preferenceNameContainer}>
                 <Text style={[ChooseOptionStyle.preferenceNameText, { color: DM("black") }]}>
                     {props.text}
                 </Text>
             </View>
-            <View style={ChooseOptionStyle.switchContainer}>
+            <View>
                 <Switch
                     style={ChooseOptionStyle.switch}
                     value={props.value}
                     onValueChange={(value) => props.onValueChanged(value)}
                 />
-            </View>
-            <View style={ChooseOptionStyle.preferenceValueContainer}>
-                <Text style={[ChooseOptionStyle.preferenceValueText, { color: DM("black") }]}>
-                    {props.value ? "Yes" : "No"}
-                </Text>
             </View>
         </View>
     );
