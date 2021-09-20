1043534eb439a1e9c6e8f8bc0896f1198822958f
Nazariy <docnazar7@gmail.com>
2021-09-20 17:12:18 +0300

    change cofig file

--- Car/react-native.config.js
+++ Car/react-native.config.js
@@ -4,5 +4,38 @@ module.exports = {
         android: {}
     },
 
+    dependencies: {
+        'react-native-exception-handler': {
+            platforms: {
+                ios: null,
+            },
+        },
+        '@react-native-community/geolocation': {
+            platforms: {
+                ios: null,
+            },
+        },
+        'react-native-image-picker': {
+            platforms: {
+                ios: null,
+            },
+        },
+        'react-native-restart': {
+            platforms: {
+                ios: null,
+            },
+        },
+        'react-native-text-gradient': {
+            platforms: {
+                ios: null,
+            },
+        },
+        'react-native-linear-gradient': {
+            platforms: {
+                ios: null,
+            },
+        },
+    },
+
     assets: [".assets/fonts/"]
 };
