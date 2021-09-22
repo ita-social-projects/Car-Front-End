c2b047973dc4d22d591305b1b85c0ea751d1bb37
Vlas2001 <81962606+Vlas2001@users.noreply.github.com>
2021-09-22 14:41:29 +0000

    Different pages have different stops view (#583)
    * Fixed stops view in notifications and journey page
    
    * Changed stops in notifications
    
    * Fixed stops view
    
    * Fixed code smells
    
    * Replaced map with foreach

--- Car/src/activity/journey/journey-activity/create-journey/CreateJourney.tsx
+++ Car/src/activity/journey/journey-activity/create-journey/CreateJourney.tsx
@@ -17,7 +17,7 @@ import {
     LEFT_PADDING_FOR_VIA_PLACEHOLDER,
     NUMBER_OF_STOPS_LIMIT
 } from "../../../../constants/JourneyConstants";
-import { DELETE_COUNT, FIRST_ELEMENT_INDEX } from "../../../../constants/GeneralConstants";
+import { DELETE_COUNT, FIRST_ELEMENT_INDEX, SECOND_ELEMENT_INDEX } from "../../../../constants/GeneralConstants";
 import APIConfig from "../../../../../api-service/APIConfig";
 import MapViewDirections from "react-native-maps-directions";
 import Geolocation from "@react-native-community/geolocation";
@@ -46,6 +46,7 @@ import JourneyDetailsPageProps from "../journey-details-page/JourneyDetailsPageP
 import { isDarkMode } from "../../../../components/theme/ThemeProvider";
 import { darkMapStyle } from "../../../../constants/DarkMapStyleConstant";
 import AddressInputButton from "../../../../components/address-input-button/AddressInputButton";
+import Stop from "../../../../../models/stop/Stop";
 
 interface CreateJourneyComponent {
     addStopPressHandler: () => void,
@@ -79,7 +80,7 @@ const CreateJourney: CreateJourneyComponent = ({ props }: { props: CreateJourney
     const [to, setTo] = useState<WayPoint>(
         journey ? mapStopToWayPoint(getStopByType(journey, StopType.Finish)) : initialWayPoint);
     const [stops, setStops] = useState<WayPoint[]>(
-        journey ? getJourneyStops(journey)!.map(mapStopToWayPoint) : []);
+        journey ? getJourneyStops(journey)!.filter(stop => stop!.userId === user!.id).map(mapStopToWayPoint) : []);
     const [duration, setDuration] = useState(journey ? journey.duration : "");
     const [routeDistance, setRouteDistance] = useState<number>(journey?.routeDistance ?? INITIAL_ROUTE_DISTANCE);
     const [routePoints, setRoutePoints] = useState<LatLng[]>(journey?.journeyPoints ?? []);
@@ -222,7 +223,7 @@ const CreateJourney: CreateJourneyComponent = ({ props }: { props: CreateJourney
     }, []);
 
     useEffect(() => {
-        CreateJourney.numberOfAddedStop = stops.length;
+        CreateJourney.numberOfAddedStop = stops.length+SECOND_ELEMENT_INDEX;
     });
 
     CreateJourney.addStopPressHandler = () => {
@@ -284,6 +285,12 @@ const CreateJourney: CreateJourneyComponent = ({ props }: { props: CreateJourney
 
         setRouteIsUpdating(true);
 
+        let arrayOfPassengerStops = journey.stops.filter(stop => stop!.userId !== user!.id);
+
+        arrayOfPassengerStops.forEach((item) => {
+            item!.id = 0; item!.address!.id = 0;
+        });
+
         const updatedJourney: JourneyDto = {
             ...journey,
             carId: journey.car!.id,
@@ -292,7 +299,8 @@ const CreateJourney: CreateJourneyComponent = ({ props }: { props: CreateJourney
             routeDistance: Math.round(routeDistance),
             journeyPoints: routePoints.map((point, index) =>
                 ({ ...point, index: index, journeyId: journey?.id })),
-            stops: createStopArrayFromWayPoint(from, to, stops, Number(user?.id), journey.id)
+            stops: (createStopArrayFromWayPoint(from, to, stops, Number(user?.id), journey.id) as Stop[]).
+                concat(arrayOfPassengerStops)
         };
 
         await JourneyService.updateRoute(updatedJourney)
