ef1b7d1e4594b25837e879f4cebd16c54a1b8aa8
Vova <Tulen2001@gmail.com>
2021-10-01 10:45:21 +0300

    Fixed badge in chats

--- Car/src/components/badge/Badge.tsx
+++ Car/src/components/badge/Badge.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Text, View } from "react-native";
 import BadgeStyle from "./BadgeStyle";
 import BadgeProps from "./BadgeProps";
+import { EMPTY_COLLECTION_LENGTH } from "../../constants/GeneralConstants";
 
 const Badge = (props: BadgeProps) => {
-    return (
+    return (props.value !== EMPTY_COLLECTION_LENGTH ?
         <View style={BadgeStyle.container}>
             <Text style={BadgeStyle.text}>{props.value ?? ""}</Text>
         </View>
+        :
+        <View></View>
     );
 };
 
