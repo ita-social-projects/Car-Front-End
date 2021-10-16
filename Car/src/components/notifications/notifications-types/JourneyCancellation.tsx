d9e85df4142c976737e12e93a1805a96b588e330
VladZavorotny <61156598+vladzavorotny@users.noreply.github.com>
2021-10-16 13:02:32 +0300

    Fixed notifications loading (#623)
    * Closes ita-social-projects/Car-Back-End/issues/750
    
    * Fixed bugs, response speed increased
    
    * Fixed code smell

--- Car/src/components/notifications/notifications-types/JourneyCancellation.tsx
+++ Car/src/components/notifications/notifications-types/JourneyCancellation.tsx
@@ -1,70 +1,22 @@
-import React, { useContext, useState } from "react";
-import MinimizedNotification from "../../minimized-notification/MinimizedNotification";
-import NotificationRideDetails from "../notification-ride-details/NotificationRideDetails";
-import NotificationButtonGroup from "../notification-buttons/NotificationButtonGroup";
-import NotificationHeader from "../notification-header/NotificationHeader";
-import NotificationModalBase from "../notification-modal-base/NotificationModalBase";
+import React, { useContext } from "react";
 import NotificationProps from "../NotificationProps";
-import NotificationConfirmButton from "../notification-buttons/NotificationConfirmButton";
-import NotificationRideStops from "../notification-ride-stops/NotificationRideStops";
 import AuthContext from "../../auth/AuthContext";
-import { onStopPressHandler } from "./StopNavigationFunction/StopNavigationFunction";
-import Stop from "../../../../models/stop/Stop";
-import JourneyPoint from "../../../../models/journey/JourneyPoint";
+import ApplicationAnswer from "./ApplicationAnswer";
 
 const JourneyCancellation = (props: NotificationProps) => {
-    const [modalVisible, setModalVisible] = useState(props.visible);
     const user = useContext(AuthContext).user;
 
-    const onStopPress = (stop:Stop, stops:Stop[], journeyPoints: JourneyPoint[], notification: NotificationProps) =>
-    {
-        setModalVisible(false);
-        onStopPressHandler(stop,stops,journeyPoints, notification);
-    };
-
     return (
-        <>
-            <MinimizedNotification
-                notificationId={props.notificationId}
-                user={props.sender}
-                notificationTitle={"Ride is canceled"}
-                read={props.read}
-                date={props.date}
-                openModal={() => setModalVisible(true)}
-            />
-            <NotificationModalBase isVisible={modalVisible!}>
-                <NotificationHeader
-                    title="RIDE IS CANCELED"
-                    message={"The driver has canceled \nyour ride!"}
-                    sender={props.sender}
-                    disableModal={() => setModalVisible(false)}
-                />
-
-                <NotificationRideDetails
-                    journeyId={props.journeyId}
-                    userId={user?.id!}
-                    IsBaggageVisible
-                    IsAvailableSeatsVisible
-                    IsFeeVisible
-                    IsDepartureTimeVisible
-                    IsDetailsTitleVisible
-                    withSeats
-                />
-
-                <NotificationRideStops
-                    title={ `${props.sender?.name}'s route`}
-                    stopsOwner={user}
-                    journeyId={props.journeyId}
-                    IsStopsTitleVisible
-                    onStopPress = {onStopPress}
-                    notification = {props}
-                />
-
-                <NotificationButtonGroup>
-                    <NotificationConfirmButton onConfirm={() => setModalVisible(false)} />
-                </NotificationButtonGroup>
-            </NotificationModalBase>
-        </>
+        <ApplicationAnswer
+            notification = {props}
+            notificationTittle = "Ride is canceled"
+            notificationHeaderTittle = "RIDE IS CANCELED"
+            notificationHeaderMessage = "The driver has canceled \nyour ride!"
+            IsDetailsTitleVisible={false}
+            IsFeeVisible={false}
+            IsBaggageVisible={false}
+            journeyUserId = {user?.id!}
+        />
     );
 };
 
--- Car/src/components/notifications/notifications-types/PassengerWithdrawal.tsx
+++ Car/src/components/notifications/notifications-types/PassengerWithdrawal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MinimizedNotification from "../../minimized-notification/MinimizedNotification";
 import NotificationRideDetails from "../notification-ride-details/NotificationRideDetails";
 import NotificationButtonGroup from "../notification-buttons/NotificationButtonGroup";
@@ -6,20 +6,57 @@ import NotificationHeader from "../notification-header/NotificationHeader";
 import NotificationModalBase from "../notification-modal-base/NotificationModalBase";
 import NotificationProps from "../NotificationProps";
 import NotificationConfirmButton from "../notification-buttons/NotificationConfirmButton";
-import NotificationRideStops from "../notification-ride-stops/NotificationRideStops";
 import { onStopPressHandler } from "./StopNavigationFunction/StopNavigationFunction";
 import Stop from "../../../../models/stop/Stop";
 import JourneyPoint from "../../../../models/journey/JourneyPoint";
+import Journey from "../../../../models/journey/Journey";
+import JourneyUserDto from "../../../../models/journey-user/JourneyUserDto";
+import JourneyService from "../../../../api-service/journey-service/JourneyService";
+import axios from "axios";
+import StopType from "../../../../models/stop/StopType";
+import { getStopByType } from "../../../utils/JourneyHelperFunctions";
+import { FIRST_ELEMENT_INDEX, SECOND_ELEMENT_INDEX, THIRD_ELEMENT_INDEX } from "../../../constants/GeneralConstants";
+import StopsBlock from "../../../activity/journey/journey-activity/journey-page/blocks/stops-block/StopsBlock";
+import { Text, View } from "react-native";
+import JourneyNewApplicantViewStyle from "../../journey-new-applicant/journey-new-applicant-view/JourneyNewApplicantViewStyle";
+import { useTheme } from "../../theme/ThemeProvider";
 
 const PassengerWithdrawal = (props: NotificationProps) => {
+    const { DM } = useTheme();
     const [modalVisible, setModalVisible] = useState(props.visible);
     const data = JSON.parse(props.notificationData);
+    const [stops, setStops] = useState<Stop[]>();
+    const [journey, setJourney] = useState<Journey>();
+    const [journeyUser, setJourneyUser] = useState<JourneyUserDto>();
+    const [wasOpened, setWasOpened] = useState(false);
+    const source = useRef(axios.CancelToken.source());
 
-    const onStopPress = (stop:Stop, stops:Stop[], journeyPoints: JourneyPoint[], notification: NotificationProps) =>
+    const onStopPress = (stop:Stop, myStops:Stop[], journeyPoints: JourneyPoint[], notification: NotificationProps) =>
     {
         setModalVisible(false);
-        onStopPressHandler(stop,stops,journeyPoints, notification);
+        onStopPressHandler(stop,myStops,journeyPoints, notification);
     };
+
+    useEffect(() => {
+        if(!wasOpened&&modalVisible)
+        {
+            setWasOpened(true);
+            JourneyService.getJourney(props.journeyId, false, { cancelToken: source.current.token })
+                .then(res => {
+                    setJourney(res.data);
+                    setJourneyUser(data.journeyUser);
+
+                    setStops([
+                    getStopByType(res.data, StopType.Start)!,
+                    res.data!.stops.filter((stop:Stop) =>
+                        stop!.index === FIRST_ELEMENT_INDEX && stop!.userId === props.sender?.id)[FIRST_ELEMENT_INDEX],
+                    res.data!.stops.filter((stop:Stop) =>
+                        stop!.index === SECOND_ELEMENT_INDEX && stop!.userId === props.sender?.id)[FIRST_ELEMENT_INDEX],
+                    getStopByType(res.data, StopType.Finish)!
+                    ]);
+                });
+        }
+    }, [modalVisible]);
 
     return (
         <>
@@ -43,17 +80,20 @@ const PassengerWithdrawal = (props: NotificationProps) => {
                 <NotificationRideDetails
                     journeyId={props.journeyId}
                     userId={props.sender?.id!}
-                    journeyUser={data.journeyUser}
+                    journeyUser={journeyUser!}
+                    journey = {journey!}
                 />
 
-                <NotificationRideStops
-                    title={"Your route"}
-                    journeyId={props.journeyId}
-                    stopsOwner={props.sender}
-                    onStopPress={onStopPress}
-                    notification={props}
-                />
-
+                <Text style={{ ...JourneyNewApplicantViewStyle.applicantStopsText, color: DM("black") }}>
+                    {props.sender!.name} {props.sender!.surname}`s stops in your ride
+                </Text>
+                <View>
+                    <StopsBlock
+                        stops={stops? stops: []}
+                        onStopPress={()=> onStopPress}
+                        highlightedStops={[SECOND_ELEMENT_INDEX, THIRD_ELEMENT_INDEX]}
+                    />
+                </View>
                 <NotificationButtonGroup>
                     <NotificationConfirmButton onConfirm={() => setModalVisible(false)} />
                 </NotificationButtonGroup>
