4ef0b6a3be5124f30aa6f9a85e64f45fb0aba60a
Hennadii Sapryka <paperoncini@gmail.com>
2021-10-25 14:09:13 +0300

    Fix bug when 'Comments' edit box isn't saved

--- Car/src/components/commentBlock/CommentBlock.tsx
+++ Car/src/components/commentBlock/CommentBlock.tsx
@@ -1,16 +1,37 @@
 import PreferencesStyle from "../../activity/my-profile/my-profile-activity/preferences/PreferencesStyle";
 import { Text, TextInput, View } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CommentBlockProps from "./CommentBlockProps";
 import CommentBlockStyle from "./CommentBlockStyle";
 import { useTheme } from "../theme/ThemeProvider";
 import { PREFERENCES_COMMENTS_MAX_LENGTH } from "../../constants/GeneralConstants";
+import AsyncStorage from "@react-native-community/async-storage";
 
 const CommentBlock = (props: CommentBlockProps) => {
     const { colors } = useTheme();
-    const [comment, setComment] = useState(props.initialComment);
+    const [comment, setComment] = useState("");
     const [remainingSymbolsText, setRemainingSymbolsText] = useState("Up to 100 symbols");
+    
+    const saveComment = async () => {
+        
+        await AsyncStorage.setItem('Key',comment);
+    }
 
+    const loadComment = async () => {
+        const setvalue = await AsyncStorage.getItem('Key')
+        if (setvalue !== null) {
+            setComment(setvalue);
+        }
+    }
+
+    useEffect(() => {
+        loadComment();
+    },[]);
+
+    useEffect(() => {
+        saveComment();
+    },[comment]);
+    
     return (
         <View style={CommentBlockStyle.commentsContainer}>
             <Text style={[PreferencesStyle.commentsText, { color: colors.hover }]}>
