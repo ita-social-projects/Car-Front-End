48b8a0d5f6df7ad35b0bc713d9f4b0153bf752f6
Stanislav Hut <stasgut1102@gmail.com>
2021-11-01 16:13:38 +0200

    indents fix

--- Car/src/activity/journey/journey-activity/journey-details-page/JourneyDetailsPage.tsx
+++ Car/src/activity/journey/journey-activity/journey-details-page/JourneyDetailsPage.tsx
@@ -472,7 +472,8 @@ const JourneyDetailsPage = (props: JourneyDetailsPageProps) => {
                                     <Text style={{ ...CreateJourneyStyle.invitationsDesctiption,
                                         color: colors.primary }}>
                                         {existingInvitations.length +
-                                            newInvitations.filter(inv => inv.isCorrect).length} SoftServians will be notidied for this Journey</Text>
+                                            newInvitations.filter(inv => inv.isCorrect).length} SoftServians will be notidied 
+                                        for this Journey</Text>
                                 </View>
                                 <Ionicons
                                     style={[
