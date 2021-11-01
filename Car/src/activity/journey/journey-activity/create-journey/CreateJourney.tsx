15474218901e31a2845f322ba269474241ffe61c
Hennadii_Sapryka <69433810+Hennadii-Sapryka@users.noreply.github.com>
2021-11-01 19:09:05 +0200

    Update CreateJourney.tsx
    fixed it

--- Car/src/activity/journey/journey-activity/create-journey/CreateJourney.tsx
+++ Car/src/activity/journey/journey-activity/create-journey/CreateJourney.tsx
@@ -52,7 +52,7 @@ import CredentialsManager from "../../../../../credentials/credentials.json";
 interface CreateJourneyComponent {
     addStopPressHandler: () => void,
     numberOfAddedStop: number,
-    //
+    
     ({ props }: { props: CreateJourneyProps }): JSX.Element
 }
 
