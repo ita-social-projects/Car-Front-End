e558865e9b300103d462b05dd2df1bc2b72588de
Mustafaev-Mustafa <Mustafaev-Mustafa@users.noreply.github.com>
2021-11-04 18:27:13 +0200

    Removed popup and elipses menu, added Remove button

--- Car/src/activity/my-profile/my-profile-activity/address-book/address-book-tabs/AddressBookTabs.tsx
+++ Car/src/activity/my-profile/my-profile-activity/address-book/address-book-tabs/AddressBookTabs.tsx
@@ -1,32 +1,21 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import React, { useRef, useState } from "react";
-import { Animated, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Animated, View } from "react-native";
 import HeaderStyle from "../../../../../components/styles/HeaderStyle";
 import AddressBook from "../AddressBook";
 import HeaderBackButton from "../../../../../components/header-back-button/HeaderBackButton";
 import { useTheme } from "../../../../../components/theme/ThemeProvider";
 import AddLocation from "../address-book-activity/add-locations/AddLocation";
 import EditLocation from "../address-book-activity/edit-locations/EditLocation";
-import HeaderEllipsis from "../../../../../components/header-ellipsis/HeaderEllipsis";
-import BottomPopup from "../../../../../components/bottom-popup/BottomPopup";
-import { EDIT_ADDRESS_MORE_OPTIONS_POPUP_HEIGHT } from "../../../../../constants/AddressConstants";
 import {
     MAX_OPACITY,
-    MAX_POPUP_POSITION,
-    MIN_POPUP_HEIGHT,
-    MIN_POPUP_POSITION,
     ZERO_OPACITY,
-    HALF_OPACITY
 } from "../../../../../constants/StylesConstants";
 import {
-    ANIMATION_DURATION,
     MODAL_SLEEP_DURATION,
-    SLEEP_DURATION,
-    animateOpacity,
     sleep
 } from "../../../../../constants/AnimationConstants";
 import { FIRST_ELEMENT_INDEX } from "../../../../../constants/GeneralConstants";
-import JourneyPageStyle from "../../../../journey/journey-activity/journey-page/JourneyPageStyle";
 import RemoveAddressButton from "../../../../../components/menu-remove-address-button/RemoveAddressButton";
 import * as navigation from "../../../../../components/navigation/Navigation";
 import ConfirmModal from "../../../../../components/confirm-modal/ConfirmModal";
@@ -37,7 +26,7 @@ const StackTabs = createStackNavigator();
 export default function AddressBookTabs () {
     const { colors } = useTheme();
     const [modalVisibility, setModalVisibility] = useState(false);
-    const [isVisible, setVisibility] = useState(false);
+    const [isVisible] = useState(false);
     const layoutOpacity = useState(new Animated.Value(ZERO_OPACITY))[FIRST_ELEMENT_INDEX];
     const addressOpacity = useState(new Animated.Value(MAX_OPACITY))[FIRST_ELEMENT_INDEX];
 
