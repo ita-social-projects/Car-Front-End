295044a3512b906a493c53634f8925f2908eb2b9
JeJoestar <gekakaraza@gmail.com>
2021-11-06 00:26:31 +0200

    Added new taxi seat constant and presets for own car and taxi

--- Car/src/activity/journey/journey-activity/journey-details-page/JourneyDetailsPage.tsx
+++ Car/src/activity/journey/journey-activity/journey-details-page/JourneyDetailsPage.tsx
@@ -15,7 +15,8 @@ import AuthContext from "../../../../components/auth/AuthContext";
 import { DAY_OFFSET, MINUTES_OFFSET } from "../../../../constants/AnimationConstants";
 import {
     CREATING_FONT_SIZE,
-    DEFAULT_AVAILABLE_SEATS_COUNT, EDITING_FONT_SIZE,
+    DEFAULT_AVAILABLE_SEATS_COUNT,
+    DEFAULT_TAXI_AVAILABLE_SEATS_COUNT, EDITING_FONT_SIZE,
     INITIAL_TIME,
     MIN_AVAILABLE_SEATS_COUNT
 } from "../../../../constants/JourneyConstants";
@@ -386,9 +387,11 @@ const JourneyDetailsPage = (props: JourneyDetailsPageProps) => {
                             onLeftButtonPress={() => {
                                 setOwnCar(true);
                                 setIsVisibleCarDropDown(false);
+                                setAvailableSeats(DEFAULT_AVAILABLE_SEATS_COUNT);
                             }}
                             onRightButtonPress={() => {
                                 setOwnCar(false);
+                                setAvailableSeats(DEFAULT_TAXI_AVAILABLE_SEATS_COUNT);
                             }}
                             title={"Ride Type"}
                             leftButtonText={"Own car"}
@@ -441,16 +444,15 @@ const JourneyDetailsPage = (props: JourneyDetailsPageProps) => {
                             title={"Passengers"}
                             minValue={journey?.participants.length ?? MIN_AVAILABLE_SEATS_COUNT}
                         />
-
-                        <View style={CommentBlockStyle.commentsContainer}>
-                            <CommentBlock
-                                initialComment={comment}
-                                commentHeader="Comments"
-                                placeholder = "Write your comments"
-                                setComments={(initialComment:string)=>
-                                    setComments(initialComment)}
-                            />
-                        </View>
+                        
+                        <CommentBlock
+                            initialComment={comments}
+                            commentHeader="Comments"
+                            placeholder = "Write your comments"
+                            setComments={(initialComment:string)=>
+                                setComments(initialComment)}
+                        />
+                        
 
                         <View style={CreateJourneyStyle.invitationsView}>
                             <Text style={[CreateJourneyStyle.commentsCaption, { color: colors.primary }]}>Invited
