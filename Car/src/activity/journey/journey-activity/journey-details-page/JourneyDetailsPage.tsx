138eebfc0dcf59ad807607079b27f7f0dc53ec30
Oleksandr <medytskyi.oleksandr@gmail.com>
2021-11-08 13:50:11 +0200

    Resolved pull request conflict

--- Car/src/activity/journey/journey-activity/journey-details-page/JourneyDetailsPage.tsx
+++ Car/src/activity/journey/journey-activity/journey-details-page/JourneyDetailsPage.tsx
@@ -470,9 +470,8 @@ const JourneyDetailsPage = (props: JourneyDetailsPageProps) => {
                                     <Text style={{ ...CreateJourneyStyle.invitationsDesctiption,
                                         color: colors.primary }}>
                                         {existingInvitations.length +
-                                            newInvitations.filter(inv => inv.isCorrect).length}
-                                            SoftServians will be notidied for this Journey
-                                    </Text>
+                                            newInvitations.filter(inv => inv.isCorrect).length} SoftServian will be
+                                            notified for that Journey</Text>
                                 </View>
                                 <Ionicons
                                     style={[
