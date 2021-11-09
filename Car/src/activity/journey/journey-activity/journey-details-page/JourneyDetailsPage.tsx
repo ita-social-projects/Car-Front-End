931e58799c58c20d45cacb3f2b3bd11dae52bdae
gnidetsOleksandr <sgnidet@gmail.com>
2021-11-09 19:28:42 +0200

    Made some changes due to tests

--- Car/src/activity/journey/journey-activity/journey-details-page/JourneyDetailsPage.tsx
+++ Car/src/activity/journey/journey-activity/journey-details-page/JourneyDetailsPage.tsx
@@ -33,7 +33,7 @@ import Indicator from "../../../../components/activity-indicator/Indicator";
 import ConfirmModal from "../../../../components/confirm-modal/ConfirmModal";
 import moment from "moment";
 import ConfirmModalProps from "../../../../components/confirm-modal/ConfirmModalProps";
-import { freeRideModal, invitationsErrorModal, paidRideModal, publishErrorModal, updateErrorModal } from "./JourneyDetailsModals";
+import { freeRideModal, invitationsErrorModal, publishErrorModal, updateErrorModal } from "./JourneyDetailsModals";
 import { createStopArrayFromWayPoint } from "../../../../utils/JourneyHelperFunctions";
 import Journey from "../../../../../models/journey/Journey";
 import AddressInputButton from "../../../../components/address-input-button/AddressInputButton";
