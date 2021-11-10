d0ac7d649c05bd35816bb3a35cc3f42170d4733d
Stanislav Hut <stasgut1102@gmail.com>
2021-11-11 00:43:03 +0200

    re-fix big/831

--- Car/src/components/commentBlock/CommentBlockStyle.tsx
+++ Car/src/components/commentBlock/CommentBlockStyle.tsx
@@ -4,7 +4,7 @@ const CommentBlockStyle = StyleSheet.create({
     commentsContainer: {
         paddingTop: 24,
         paddingLeft: 16,
-        paddingRight: 15
+        paddingRight: 15,
     },
 });
 
