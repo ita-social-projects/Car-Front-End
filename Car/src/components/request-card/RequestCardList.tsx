fc550b9b74e7d7d4cf2e208f91cc99eb88e3fe66
Stanislav Hut <stasgut1102@gmail.com>
2022-01-06 17:51:46 +0200

    Request address fix

--- Car/src/components/request-card/RequestCardList.tsx
+++ Car/src/components/request-card/RequestCardList.tsx
@@ -14,6 +14,19 @@ interface RequestCardListProps {
 const RequestCardList = (props:RequestCardListProps) => {
     const request: Request[] = props.request;
     const isPast: boolean = props.isPast ?? false;
+
+    request.sort((a: Request, b: Request) => compare(a,b, props.ascending));
+
+    function compare (a: Request, b: Request, ascendingOrder?:boolean) {
+        if (a?.departureTime! < b?.departureTime!){
+            return ascendingOrder?LESS_THAN_ZERO:MORE_THAN_ZERO;
+        }
+        if (a?.departureTime! > b?.departureTime!){
+            return ascendingOrder?MORE_THAN_ZERO:LESS_THAN_ZERO;
+        }
+
+        return ZERO;
+    }
     
     return (
         <View>
