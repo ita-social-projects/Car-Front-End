ee178440b0382b507cce7d64aadeb45af4a6eeb5
Andriy Yatsula <yauund1@gmail.com>
2022-01-24 16:48:03 +0200

    Fixed bug/984

--- Car/src/activity/journey/journey-activity/address-input-page/AddressInputPageStyle.tsx
+++ Car/src/activity/journey/journey-activity/address-input-page/AddressInputPageStyle.tsx
@@ -3,9 +3,10 @@ import { StyleSheet } from "react-native";
 const AddressInputPageStyle = StyleSheet.create({
     inputContainer: {
         position: "absolute",
-        top: 32,
+        top: 9,
         left: 10,
         right: 10,
+        width:"85%"
     },
     mapContainer: {
         zIndex: -2,
