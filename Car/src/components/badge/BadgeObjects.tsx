f2f2f8b28474be598669c95e73f72215e858751e
victoria <victoria603111@gmail.com>
2022-02-04 00:26:07 +0200

    Names of images and badge props change

--- Car/src/components/badge/BadgeObjects.tsx
+++ Car/src/components/badge/BadgeObjects.tsx
@@ -1,4 +1,5 @@
 import BadgeProps from "./BadgeProps";
+import BadgeTypes from "./BadgeTypes";
 
 interface BadgesArray {
     [index: number] : BadgeProps
@@ -7,155 +8,136 @@ interface BadgesArray {
 var Badges: BadgesArray = [
     {
         name: "beginner",
+        type: BadgeTypes.passangerRides,
         description: "Yay, you've rided 1 ride!",
-        pathLocked: "../../../assets/Badges/Locked/PassRides_1",
-        pathUnlocked: "../../../assets/Badges/Unlocked/PassRides_1",
         points: 1,
-        isAchived: false
+        isReached: false
     },
     {
         name: "freelancer",
         description: "Yay, you've rided 5 rides!",
-        pathLocked: "../../../assets/Badges/Locked/PassRides_5",
-        pathUnlocked: "../../../assets/Badges/Unlocked/PassRides_5",
+        type: BadgeTypes.passangerRides,
         points: 5,
-        isAchived: false
+        isReached: false
     },
     {
         name: "turist",
         description: "Yay, you've rided 10 rides!",
-        pathLocked: "../../../assets/Badges/Locked/PassRides_10",
-        pathUnlocked: "../../../assets/Badges/Unlocked/PassRides_10",
+        type: BadgeTypes.passangerRides,
         points: 10,
-        isAchived: false
+        isReached: false
     },
     {
         name: "Ride Buddy",
         description: "Yay, you've rided 20 rides!",
-        pathLocked: "../../../assets/Badges/Locked/PassRides_20",
-        pathUnlocked: "../../../assets/Badges/Unlocked/PassRides_20",
+        type: BadgeTypes.passangerRides,
         points: 20,
-        isAchived: false
+        isReached: false
     },
     {
         name: "frog traveler",
         description: "Yay, you've rided 50 rides!",
-        pathLocked: "../../../assets/Badges/Locked/PassRides_50",
-        pathUnlocked: "../../../assets/Badges/Unlocked/PassRides_50",
+        type: BadgeTypes.passangerRides,
         points: 50,
-        isAchived: false
+        isReached: false
     },
     {
         name: "travel guru",
         description: "Yay, you've rided 100 rides!",
-        pathLocked: "../../../assets/Badges/Locked/PassRides_100",
-        pathUnlocked: "../../../assets/Badges/Unlocked/PassRides_100",
+        type: BadgeTypes.passangerRides,
         points: 100,
-        isAchived: false
+        isReached: false
     },
     {
         name: "beginner",
         description: "Yay, you've rided 1 ride!",
-        pathLocked: "../../../assets/Badges/Locked/DriverRides_1",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverRides_1",
+        type: BadgeTypes.DriverRides,
         points: 1,
-        isAchived: false
+        isReached: false
     },
     {
         name: "freelancer",
         description: "Yay, you've rided 5 rides!",
-        pathLocked: "../../../assets/Badges/Locked/DriverRides_5",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverRides_5",
+        type: BadgeTypes.DriverRides,
         points: 5,
-        isAchived: false
+        isReached: false
     },
     {
         name: "ridoholic",
         description: "Yay, you've rided 10 rides!",
-        pathLocked: "../../../assets/Badges/Locked/DriverRides_10",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverRides_10",
+        type: BadgeTypes.DriverRides,
         points: 10,
-        isAchived: false
+        isReached: false
     },
     {
         name: "sachem",
         description: "Yay, you've rided 20 rides!",
-        pathLocked: "../../../assets/Badges/Locked/DriverRides_20",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverRides_20",
+        type: BadgeTypes.DriverRides,
         points: 20,
-        isAchived: false
+        isReached: false
     },
     {
         name: "super driver",
         description: "Yay, you've rided 50 rides!",
-        pathLocked: "../../../assets/Badges/Locked/DriverRides_50",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverRides_50",
+        type: BadgeTypes.DriverRides,
         points: 50,
-        isAchived: false
+        isReached: false
     },
     {
         name: "jet driver",
         description: "Yay, you've rided 100 rides!",
-        pathLocked: "../../../assets/Badges/Locked/DriverRides_100",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverRides_100",
+        type: BadgeTypes.DriverRides,
         points: 100,
-        isAchived: false
+        isReached: false
     },
     {
         name: "SpaceX driver",
         description: "Yay, you've rided 200 rides!",
-        pathLocked: "../../../assets/Badges/Locked/DriverRides_200",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverRides_200",
+        type: BadgeTypes.DriverRides,
         points: 200,
-        isAchived: false
+        isReached: false
     },
     {
         name: "elusive Joe",
         description: "Yay, you've rided 10 km!",
-        pathLocked: "../../../assets/Badges/Locked/DriverKm_10",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverKm_10",
+        type: BadgeTypes.DriverKm,
         points: 10,
-        isAchived: false
+        isReached: false
     },
     {
         name: "sprinter",
         description: "Yay, you've rided 50 km!",
-        pathLocked: "../../../assets/Badges/Locked/DriverKm_50",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverKm_50",
+        type: BadgeTypes.DriverKm,
         points: 50,
-        isAchived: false
+        isReached: false
     },
     {
         name: "marathon driver",
         description: "Yay, you've rided 100 km!",
-        pathLocked: "../../../assets/Badges/Locked/DriverKm_100",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverKm_100",
+        type: BadgeTypes.DriverKm,
         points: 100,
-        isAchived: false
+        isReached: false
     },
     {
         name: "scorcher",
         description: "Yay, you've rided 200 km!",
-        pathLocked: "../../../assets/Badges/Locked/DriverKm_200",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverKm_200",
+        type: BadgeTypes.DriverKm,
         points: 200,
-        isAchived: false
+        isReached: false
     },
     {
         name: "007",
         description: "Yay, you've rided 300 km!",
-        pathLocked: "../../../assets/Badges/Locked/DriverKm_300",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverKm_300",
+        type: BadgeTypes.DriverKm,
         points: 300,
-        isAchived: false
+        isReached: false
     },
     {
         name: "Santa",
         description:"Yay, you've rided 500 km!",
-        pathLocked: "../../../assets/Badges/Locked/DriverKm_500",
-        pathUnlocked: "../../../assets/Badges/Unlocked/DriverKm_500",
+        type: BadgeTypes.DriverKm,
         points: 500,
-        isAchived: false
+        isReached: false
     }
 ];
 
