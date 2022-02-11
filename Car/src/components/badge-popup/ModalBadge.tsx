c147a1bb9878c9605e5f2b9a9548c1f065d10403
MaksymTertychnyj <maka12008@ukr.net>
2022-02-11 20:15:30 +0200

    task/#1014 (create components ModalBadge, ModalBadgeStyles)

---
+++ Car/src/components/badge-popup/ModalBadge.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useRef, useState } from "react";
+import { View, Modal, Animated, Text, Image, TouchableOpacity } from "react-native";
+import { navigate } from "../navigation/Navigation";
+import { useTheme } from "../theme/ThemeProvider";
+import BadgePopupStyles from "./ModalBadgeStyles";
+
+const ModalBadge = ({ visible, image, closeFunc }) => {
+
+    const { colors } = useTheme();
+    const [showModal, setShowModal] = useState(visible);
+    let CLOSE_ANIMATION = 0;
+    let OPEN_ANIMATION = 200;
+
+    const scaleValue = useRef(new Animated.Value(CLOSE_ANIMATION)).current;
+
+    useEffect(() => {
+        toggleModal();
+    }, [visible]);
+
+    const toggleModal = () => {
+        if(visible){
+            setShowModal(true);
+            Animated.spring(scaleValue,
+                {
+                    toValue: 1,
+                    useNativeDriver: true
+                }).start();
+        }
+        else{
+            setTimeout(() => setShowModal(false), OPEN_ANIMATION);
+            Animated.timing(scaleValue, {
+                toValue: 0,
+                duration: 300,
+                useNativeDriver: true
+            }).start();
+        }
+    };
+
+    return (
+        <Modal transparent visible = {showModal}>
+            <View style={[BadgePopupStyles.modalBackGround]}>
+                <Animated.View style={[BadgePopupStyles.modalContainer, { transform:[{ scale: scaleValue }],
+                    backgroundColor: colors.white }]}>
+
+                    <View style={{ marginTop:32 }}>
+               		 <Text style = {[BadgePopupStyles.headerText, { color: colors.primary }]}>CONGRATULATIONS!</Text>
+           		</View>
+	            <View style={{ alignItems: "center" }}>
+	                <Image
+	                    source={image}
+	                    resizeMode = "contain"
+	                    style = {BadgePopupStyles.image}
+	                />
+	            </View>
+	            <View style={{ alignItems: "center", marginTop: 166 }}>
+	                <Text style = {[BadgePopupStyles.messageText, { color: colors.secondaryDark }]}>
+						You have a new badge!
+	                </Text>
+	                	<View style={[BadgePopupStyles.header, { marginTop:50 }]}>
+	                   		 <TouchableOpacity
+	                      		  style={[BadgePopupStyles.button]}
+	                        	  onPress={()=>{
+                                    closeFunc();
+                                    navigate("MyProfileTabs", { screen: "Badges", initial: false, });
+                                }}
+	                         >
+	                     		<Text style={BadgePopupStyles.buttonText}>See more</Text>
+	                         </TouchableOpacity>
+							 <TouchableOpacity
+	                      		  style={[BadgePopupStyles.button, { backgroundColor: colors.white, marginTop: 5 }]}
+	                        	  onPress={closeFunc}
+	                         >
+	                     		<Text style={[BadgePopupStyles.buttonText, { color: colors.primary }]}>Close</Text>
+	                         </TouchableOpacity>
+	                </View>
+	            </View>
+
+                </Animated.View>
+            </View>
+        </Modal>
+    );
+};
+
+export default ModalBadge;
---
+++ Car/src/components/badge-popup/ModalBadgeStyles.tsx
@@ -0,0 +1,75 @@
+import { StyleSheet } from "react-native";
+import Font from "../../data/fonts/Font";
+
+const BadgePopupStyles = StyleSheet.create({
+
+    modalBackGround: {
+        flex: 1,
+        backgroundColor: "rgba(0,0,0,0.5)",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    modalContainer: {
+        width: 344,
+        height: 398,
+        paddingHorizontal: 20,
+        borderRadius: 16,
+        elevation: 20
+    },
+
+    header: {
+        width: "100%",
+        height: 40,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+
+    headerText: {
+        textAlign:"center",
+        fontSize:14,
+        fontWeight:"bold",
+        fontFamily: Font.Milliard.Milliard,
+        letterSpacing: 0.25
+    },
+
+    messageText: {
+        textAlign:"center",
+        fontSize: 16,
+        fontWeight: "normal",
+        fontFamily: Font.OpenSans.OpenSans,
+        letterSpacing: 0.25,
+        lineHeight: 22
+    },
+
+    bodyText: {
+        fontSize: 16,
+        textAlign: "center",
+        color:"#909095",
+        fontFamily: Font.OpenSans.Regular
+    },
+
+    button: {
+        width: 304,
+        height: 56,
+        alignItems: "center",
+        backgroundColor: "#D80056",
+        paddingVertical: 18
+    },
+
+    buttonText: {
+        fontSize: 16,
+        textAlign: "center",
+        color:"white",
+        fontWeight: "bold",
+        fontFamily: Font.Milliard.Milliard
+    },
+
+    image: {
+        width: 137,
+        height: 150,
+        position: "absolute"
+    }
+});
+
+export default BadgePopupStyles;
