76274ba34951f63973919822bd55353e344cc0bc
RomanVynarovych <roman.vynarovych@gmail.com>
2022-03-13 21:51:31 +0200

    refactored code

--- Car/src/components/unread-messages/UnreadMessages.tsx
+++ Car/src/components/unread-messages/UnreadMessages.tsx
@@ -5,12 +5,14 @@ import Props from "./UnreadMessagesProps";
 import { useTheme } from "../theme/ThemeProvider";
 const UnreadMessages = (props: Props) => {
     const { colors } = useTheme();
+
     return (
-        <View style={[Style.container, {backgroundColor: colors.accentOrange}]}>
-            <Text style={[Style.text, {color: colors.white}]}>
+        <View style={[Style.container, { backgroundColor: colors.accentOrange }]}>
+            <Text style={[Style.text, { color: colors.white }]}>
                 {props.value ?? ""}
             </Text>
         </View>
     );
 };
+
 export default UnreadMessages;
