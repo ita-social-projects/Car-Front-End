6fdc0ed4d92fdddc2f18c692b2c14973f0eee608
Vitalii Oryshchyn <vitalii.oryshchyn@gmail.com>
2022-03-18 21:39:30 +0200

    Fixed bug #1060

--- Car/src/components/notifications/notification-buttons/NotificationButtonGroup.tsx
+++ Car/src/components/notifications/notification-buttons/NotificationButtonGroup.tsx
@@ -1,10 +1,13 @@
 import React, { ReactNode } from "react";
 import { View } from "react-native";
+import { useTheme } from "../../theme/ThemeProvider";
 import NotificationButtonGroupStyle from "./NotificationButtonGroupStyle";
 
 const NotificationButtonGroup = (props: {children?: ReactNode}) => {
+    const { colors } = useTheme();
+
     return (
-        <View style={[NotificationButtonGroupStyle.container]}>
+        <View style={[NotificationButtonGroupStyle.container, { borderTopColor: colors.disableBack }]}>
             {props.children}
         </View>
     );
