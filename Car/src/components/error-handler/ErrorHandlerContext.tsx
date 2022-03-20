b1645f55aa730d804b491fa4b0beb6a75db4c519
MaksymTertychnyj <maka12008@ukr.net>
2022-03-20 21:12:30 +0200

    Revert "task/1063 (resolved bug of SonarCloud)"
    This reverts commit 9b0ec325095539a17e0c954c63ecfec9e63b40cb.

--- Car/src/components/error-handler/ErrorHandlerContext.tsx
+++ Car/src/components/error-handler/ErrorHandlerContext.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ErrorHandlerContext = React.createContext({
     show: false,
-    setShowPopup: (a: boolean) => {a!=undefined;}
+    setShowPopup: (action: boolean) => {action;}
 });
 
 export default ErrorHandlerContext;
