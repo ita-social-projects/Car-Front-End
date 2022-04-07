55976ed4cdda5d34399c983b2ef3f8737e148dc7
yyatsuu <87204981+yyatsuu@users.noreply.github.com>
2022-04-07 17:49:21 +0300

    Update AvatarLogoTitleStyle.tsx

--- Car/src/components/avatar-logo-title/AvatarLogoTitleStyle.tsx
+++ Car/src/components/avatar-logo-title/AvatarLogoTitleStyle.tsx
@@ -11,13 +11,6 @@ const AvatarLogoTitleStyle = StyleSheet.create({
         paddingVertical: 24,
     },
 
-    container: {
-        minHeight: 48,
-        width: Dimensions.get("screen").width - 32,
-        left:20,
-        top:10
-    },
-
     headerContainer: {
         flex: 1,
         flexDirection: "row",
